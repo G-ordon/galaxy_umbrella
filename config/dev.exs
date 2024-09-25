6b35f31bdb84e031cf34c00fdc04b53c47803c44
G-ordon <gordonochieng5454@gmail.com>
2024-09-25 19:41:07 +0300

    no change.

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,7 @@ config :galaxy, Galaxy.Repo,
   database: "galaxy_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 500
+  pool_size: 10
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
